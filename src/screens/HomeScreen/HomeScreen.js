34d912865c51f6996730fe966110bc3b86604444
Paul Laptiev <paullaptiev@Pauls-MacBook-Pro.local>
2020-01-22 11:49:34 +0200

    created design for get all tours part

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -5,15 +5,17 @@ import {
   BackHandler,
   SafeAreaView,
   StatusBar,
-  AsyncStorage,
-  FlatList,
   Image,
   Switch,
   Animated
 } from 'react-native';
+import SwitchToggle from "react-native-switch-toggle";
+
 import { Icon } from 'react-native-elements';
 import { SwipeListView } from 'react-native-swipe-list-view';
-
+import {
+  Grayscale
+} from 'react-native-color-matrix-image-filters';
 class HomeScreen extends Component {
   constructor(props) {
     super(props);
@@ -21,13 +23,14 @@ class HomeScreen extends Component {
 
     this.state = {
       allToursArrraysBlat: [
-        { media_url: "Pankaj", tour_location: "Yerevan, Armenia", post_time: "2019:12:28 06:18", mediaID: "", tourID: "1" },
-        { media_url: "Pankaj", tour_location: "Yerevan, Armenia", post_time: "2019:12:28 06:18", mediaID: "", tourID: "1" },
-        { media_url: "Pankaj", tour_location: "Yerevan, Armenia", post_time: "2019:12:28 06:18", mediaID: "", tourID: "1" },
-        { media_url: "Pankaj", tour_location: "Yerevan, Armenia", post_time: "2019:12:28 06:18", mediaID: "", tourID: "1" },
+        { media_url: "Pankaj", tour_location: "Yerevan, Armenia", post_time: "2019:12:28 06:18", mediaID: "", tourID: "1", isActive: 0 },
+        { media_url: "Pankaj", tour_location: "Yerevan, Armenia", post_time: "2019:12:28 06:18", mediaID: "", tourID: "1", isActive: 0 },
+        { media_url: "Pankaj", tour_location: "Yerevan, Armenia", post_time: "2019:12:28 06:18", mediaID: "", tourID: "1", isActive: 0 },
+        { media_url: "Pankaj", tour_location: "Yerevan, Armenia", post_time: "2019:12:28 06:18", mediaID: "", tourID: "1", isActive: 0 },
       ],
       animatedValue: new Animated.Value(0),
-      isVisibleBackgroundOfFlatSwipe: false
+      isVisibleBackgroundOfFlatSwipe: false,
+      grayscaleValue: 0
     };
 
   }
@@ -43,16 +46,27 @@ class HomeScreen extends Component {
         style={[{ transform: [{ translateX }] }]}
       >
         <View>
-          <Image
-            style={{ width: "100%" }}
-            source={require('../../../download.jpeg')}
-          />
+          <Grayscale amount={item.isActive}>
+            <Image
+              style={{ width: "100%" }}
+              source={require('../../../download.jpeg')}
+            />
+          </Grayscale>
+          <View style={{ position: 'absolute', bottom: 0, backgroundColor: 'rgba(150,150,150,0.3)', width: '100%' }}>
+            <Text style={{ color: 'white', fontSize: 30 }}>Yerevan, Armenia</Text>
+            <Text style={{ color: 'white', fontSize: 18 }}>2019:12:28 06:18</Text>
+          </View>
         </View>
       </Animated.View>
     )
   }
 
-  _renderHiddenItem = () => {
+  onPress1 = (item) => {
+    item.isActive = !item.isActive
+    this.forceUpdate()
+  };
+
+  _renderHiddenItem = ({ item }) => {
 
     this.delayValue = this.delayValue + 500;
     const translateX = this.state.animatedValue.interpolate({
@@ -61,41 +75,59 @@ class HomeScreen extends Component {
     });
     return (
       <Animated.View
-      style={[{ transform: [{ translateX }] }]}
-    >
-      <View
-        style={{
-          height: '100%',
-          alignItems: 'flex-end'
-        }}>
-        <View style={{
-          width: "100%",
-          height: '50%',
-          backgroundColor: "black",
-          alignItems: 'flex-end',
-          justifyContent: 'center',
-        }}>
-          <Icon
-            name="md-trash"
-            type="ionicon"
-            color="white"
-            size={32}
-            onPress={() => onPress()}
-            underlayColor="transparent"
-          />
+        style={[{ transform: [{ translateX }] }]}
+      >
+        <View
+          style={{
+            height: '100%',
+            alignItems: 'flex-end',
+          }}>
+          <View style={{
+            width: "100%",
+            height: '50%',
+            backgroundColor: "#ff5252",
+            alignItems: 'flex-end',
+            justifyContent: 'center',
+            paddingRight: 25
+          }}>
+            <Icon
+              name="md-trash"
+              type="ionicon"
+              color="white"
+              size={32}
+              onPress={() => onPress()}
+              underlayColor="transparent"
+            />
+          </View>
+          <View style={{
+            width: "100%",
+            height: '50%',
+            backgroundColor: "grey",
+            alignItems: 'flex-end',
+            justifyContent: 'center',
+            paddingRight: 15
+
+          }}>
+            <SwitchToggle
+              containerStyle={{
+                marginTop: 16,
+                width: 40,
+                height: 20,
+                borderRadius: 25,
+                backgroundColor: "#ccc",
+                padding: 2
+              }}
+              circleStyle={{
+                width: 15,
+                height: 15,
+                borderRadius: 19,
+                backgroundColor: "white" // rgb(102,134,205)
+              }}
+              switchOn={item.isActive} onPress={() => this.onPress1(item)} />
+          </View>
         </View>
-        <View style={{
-          width: "100%",
-          height: '50%',
-          backgroundColor: "grey",
-          alignItems: 'flex-end',
-          justifyContent: 'center',
-        }}>
-          <Switch></Switch>
-        </View>
-      </View>
       </Animated.View>
-      );
+    );
   }
 
 
